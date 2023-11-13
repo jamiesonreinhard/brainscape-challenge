6bb4d9122c9bb181a3d61a4e15b7e65ea5bf9b92
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2023-11-13 16:38:47 -0600

    try to dynamically set app height for mobile screens

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Collection from "./components/imageCollection/Collection";
 import UserGallery from "./components/userGallery/UserGallery";
 
 const App = () => {
+  const [appHeight, setAppHeight] = useState(window.innerHeight);
+  const mobileBreakpoint = 768;
   const [showDropZone, setShowDropZone] = useState(false);
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < mobileBreakpoint) {
+        setAppHeight(window.innerHeight);
+      } else {
+        // Reset to default for non-mobile screens
+        setAppHeight('auto');
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    // Set the initial height
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div className="w-[90%] h-[90vh] sm:h-auto xl:max-w-[1200px] mx-auto py-[24px] sm:py-[60px] flex flex-col relative">
+    <div className="w-[90%] xl:max-w-[1200px] mx-auto py-[24px] sm:py-[60px] flex flex-col relative" style={{ height: appHeight }}>
       <header className="flex flex-col gap-[8px] mb-[40px]">
         <h2 className="text-xl font-bold">Brainscape App Coding Exercise</h2>
         <p className="text-sm">Photo Album Generator by Jamieson Reinhard</p>
       </header>
       <section className="w-full flex flex-col sm:flex-row sm:items-start overflow-hidden gap-[12px] sm:overflow-visible">
         {/* api photos component */}
-        <div className="h-1/2 sm:h-auto overflow-y-auto w-full sm:w-2/3 lg:w-fit">
-          <Collection setShowDropZone={setShowDropZone} />
-        </div>
+        <Collection setShowDropZone={setShowDropZone} />
 
         {/* selected photos component */}
         <UserGallery showDropZone={showDropZone} />
