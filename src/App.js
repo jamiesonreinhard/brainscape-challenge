8b8672f6405e0c8631cbbd9df9fad692a2cdf964
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2023-11-14 08:43:54 -0600

    replicated existing drag/drop functionality using react-dnd library

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 import Collection from "./components/imageCollection/Collection";
 import UserGallery from "./components/userGallery/UserGallery";
 import Toast from "./components/utility/Toast";
-import debounce from 'lodash/debounce';
+import debounce from "lodash/debounce";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { DndProvider } from "react-dnd";
 
 const App = () => {
   const [appHeight, setAppHeight] = useState(window.innerHeight);
@@ -12,36 +14,45 @@ const App = () => {
 
   useEffect(() => {
     const handleResize = debounce(() => {
-      const newHeight = window.innerWidth < mobileBreakpoint ? window.innerHeight : 'auto';
+      const newHeight =
+        window.innerWidth < mobileBreakpoint ? window.innerHeight : "auto";
       if (appHeight !== newHeight) {
         setAppHeight(newHeight);
       }
     }, 100);
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
 
     return () => {
       handleResize.cancel(); // Cancel any pending debounces on cleanup
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, [appHeight]);
 
   return (
-    <div className="w-[90%] xl:max-w-[1200px] mx-auto py-[24px] sm:py-[60px] flex flex-col relative" style={{ height: appHeight }}>
-      <header className="flex flex-col gap-[8px] mb-[40px]">
-        <h2 className="text-xl font-bold">Brainscape App Coding Exercise</h2>
-        <p className="text-sm">Photo Album Generator by Jamieson Reinhard</p>
-      </header>
-      <section className="w-full flex flex-col sm:flex-row sm:items-start overflow-hidden gap-[12px] sm:overflow-visible">
-        {/* api photos component */}
-        <Collection setShowDropZone={setShowDropZone} />
+    <DndProvider backend={HTML5Backend}>
+      <div
+        className="w-[90%] xl:max-w-[1200px] mx-auto py-[24px] sm:py-[60px] flex flex-col relative"
+        style={{ height: appHeight }}
+      >
+        <header className="flex flex-col gap-[8px] mb-[40px]">
+          <h2 className="text-xl font-bold">Brainscape App Coding Exercise</h2>
+          <p className="text-sm">Photo Album Generator by Jamieson Reinhard</p>
+        </header>
+        <section className="w-full flex flex-col sm:flex-row sm:items-start overflow-hidden gap-[12px] sm:overflow-visible">
+          {/* api photos component */}
+          <Collection setShowDropZone={setShowDropZone} />
 
-        {/* selected photos component */}
-        <UserGallery showDropZone={showDropZone} setShowToast={setShowToast} />
-      </section>
-      {showToast && <Toast message={showToast} />}
-    </div>
+          {/* selected photos component */}
+          <UserGallery
+            showDropZone={showDropZone}
+            setShowToast={setShowToast}
+          />
+        </section>
+        {showToast && <Toast message={showToast} />}
+      </div>
+    </DndProvider>
   );
 };
 
--- src/components/userGallery/UserGallery.js
+++ src/components/userGallery/UserGallery.js
@@ -1,24 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DropZone from "./DropZone";
 import StoredImage from "./StoredImage";
+import { useDrop } from "react-dnd";
 
 const UserGallery = ({ showDropZone, setShowToast }) => {
   const [storedImages, setStoredImages] = useState(() => {
     return JSON.parse(sessionStorage.getItem("storedImages")) || [];
   });
 
-  const handleDrop = (droppedImage) => {
-    const storedImageIds = storedImages.map((image) => image.id);
-    if (!storedImageIds.includes(droppedImage.id)) {
-      const newStoredImages = [...storedImages, droppedImage];
-      setStoredImages(newStoredImages);
-      sessionStorage.setItem("storedImages", JSON.stringify(newStoredImages));
-    } else {
-      setShowToast("Image already added.");
-      setTimeout(() => {
-        setShowToast(false);
-      }, 3000);
-    }
+  const [, dropRef] = useDrop(() => ({
+    accept: "image",
+    drop: (item) => handleDrop(item),
+  }));
+
+  const handleDrop = (item) => {
+    setStoredImages((prevStoredImages) => {
+      const storedImageIds = prevStoredImages.map((image) => image.id);
+      console.log(`Stored Image Ids: ${storedImageIds}`);
+      console.log(`Dropped Image Id: ${item.id}`);
+      if (!storedImageIds.includes(item.id)) {
+        const newStoredImages = [...prevStoredImages, item];
+        console.log(`New stored images: ${newStoredImages}`);
+        sessionStorage.setItem("storedImages", JSON.stringify(newStoredImages));
+        return newStoredImages;
+      } else {
+        setShowToast("Image already added.");
+        setTimeout(() => {
+          setShowToast(false);
+        }, 3000);
+        return prevStoredImages;
+      }
+    });
   };
 
   const removeAll = () => {
@@ -35,11 +47,20 @@ const UserGallery = ({ showDropZone, setShowToast }) => {
   };
 
   return (
-    <div className="sticky top-[60px] sm:top-[40px] right-0 w-full min-w-[300px] sm:w-1/3 lg:w-[380px] h-1/2 sm:h-[calc(100vh-200px)] overflow-y-auto bg-brainscapeBlue p-[16px]">
+    <div
+      ref={dropRef}
+      className="sticky top-[60px] sm:top-[40px] right-0 w-full min-w-[300px] sm:w-1/3 lg:w-[380px] h-1/2 sm:h-[calc(100vh-200px)] overflow-y-auto bg-brainscapeBlue p-[16px]"
+    >
       <div className="flex items-center w-full justify-between mb-[16px]">
         <p className="text-black font-bold">Album Generator</p>
-        {storedImages?.length > 0 && <button className="py-1 px-2 flex items-center bg-black rounded-[8px] text-white font-bold" onClick={removeAll}>Remove All</button>}
-        
+        {storedImages?.length > 0 && (
+          <button
+            className="py-1 px-2 flex items-center bg-black rounded-[8px] text-white font-bold"
+            onClick={removeAll}
+          >
+            Remove All
+          </button>
+        )}
       </div>
 
       <div className="flex w-full items-start justify-between gap-[8px]">
