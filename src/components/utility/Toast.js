c13220e0161d945ebf52c69feacc74540670ce8c
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2023-11-13 18:45:56 -0600

    bold text, remove all button

--- src/components/utility/Toast.js
+++ src/components/utility/Toast.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Toast = ({message}) => {
   return (
     <div className="absolute top-[16px] left-1/2 -translate-x-1/2 py-[12px] px-[24px] rounded-[8px] bg-brainscapeBlue flex items-center justify-center">
-      <span className="text-white">{message}</span>
+      <span className="text-white font-bold">{message}</span>
     </div>
   );
 };
